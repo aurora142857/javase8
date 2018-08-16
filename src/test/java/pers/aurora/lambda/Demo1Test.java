9ae940f568aaf80ee5c1c2040143ce2c8c3ae6b2
aurora <1195873214@qq.com>
2018-08-17 00:26:29 +0800

    add lambda test

---
+++ src/test/java/pers/aurora/lambda/Demo1Test.java
@@ -0,0 +1,11 @@
+package pers.aurora.lambda;
+
+import org.junit.Test;
+
+public class Demo1Test {
+    @Test
+    public void test_m1() {
+        Demo1 demo1 = new Demo1();
+        demo1.method1();
+    }
+}
